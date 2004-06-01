6443d258d319c81d5db17b2377b2ec99afc41821
niemeyer <>
2004-06-01 22:09:58 +0000

    - Removed old structures (loaders and packagemanagers). What were they   still doing here? - Added packageflags.py, missing from the last transaction.

---
+++ cpm/packageflags.py
@@ -0,0 +1,41 @@
+
+class PackageFlags(object):
+    def __init__(self):
+        self._flags = {}
+        self._matchers = {}
+
+    def add(self, flag, name, version=None, relation=None):
+        names = self._flags.get(flag)
+        if names:
+            lst = names.get(name)
+            if lst:
+                lst.append((relation, version))
+            else:
+                names[name] = [(relation, version)]
+        else:
+            self._flags[flag] = {name: [(relation, version)]}
+
+    def test(self, flag, pkg):
+        names = self._flags.get(flag)
+        if names:
+            lst = names.get(pkg.name)
+            if lst:
+                for item in lst:
+                    if pkg.matches(*item):
+                        return True
+        return False
+
+    def filter(self, flag, pkgs):
+        fpkgs = []
+        names = self._flags.get(flag)
+        if names:
+            for pkg in pkgs:
+                lst = names.get(pkg.name)
+                if lst:
+                    for item in lst:
+                        if pkg.matches(*item):
+                            fpkgs.append(pkg)
+                            break
+        return fpkgs
+
+# vim:ts=4:sw=4:et
--- cpm/packagemanager.py
+++
@@ -1,6 +0,0 @@
-
-class PackageManager:
-    def commit(self, trans):
-        pass
-
-# vim:ts=4:sw=4:et
