32efa633b84c2aab362ce59c42c40a26ed52e587
niemeyer <>
2004-08-17 23:10:53 +0000

    Adding license information.

--- gepeto/control.py
+++ gepeto/control.py
@@ -1,3 +1,24 @@
+#
+# Copyright (c) 2004 Conectiva, Inc.
+#
+# Written by Gustavo Niemeyer <niemeyer@conectiva.com>
+#
+# This file is part of Gepeto.
+#
+# Gepeto is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Gepeto is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Gepeto; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
 from gepeto.transaction import ChangeSet, ChangeSetSplitter, INSTALL, REMOVE
 from gepeto.util.strtools import strToBool
 from gepeto.channel import createChannel
@@ -116,7 +137,8 @@ class Control:
         progress.start()
         steps = 0
         for channel in channels:
-            steps += channel.getFetchSteps()
+            if manual or not channel.getManualUpdate():
+                steps += channel.getFetchSteps()
         progress.set(0, steps)
         for channel in channels:
             self._cache.removeLoader(channel.getLoader())
