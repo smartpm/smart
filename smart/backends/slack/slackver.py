5a6b8d4ecce05cab705534f84f34f98c71c76611
niemeyer <>
2005-08-31 21:52:08 +0000

    Fixing unavailble symbol on slackver.py.

--- smart/backends/slack/slackver.py
+++ smart/backends/slack/slackver.py
@@ -19,6 +19,7 @@
 # along with Smart Package Manager; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
+from smart import *
 import re
 
 VERRE = re.compile("([^-]+)(?:-([^-]+)(?:-(.+))?)?")
