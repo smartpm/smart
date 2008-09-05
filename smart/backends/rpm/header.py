4f3a89ffea50e7fb329871883bd0aec84e8c2b69
Anders F Bjorklund <afb@users.sourceforge.net>
2008-09-05 21:19:53 +0200

    support new filenames, backport from sandbox

--- smart/backends/rpm/header.py
+++ smart/backends/rpm/header.py
@@ -141,7 +141,13 @@ class RPMHeaderPackageInfo(PackageInfo):
     def getPathList(self):
         if self._path is None:
             paths = self._h[rpm.RPMTAG_OLDFILENAMES]
-            if type(paths) != list:
+            if not paths:
+                dirs = self._h[rpm.RPMTAG_DIRNAMES]
+                base = self._h[rpm.RPMTAG_BASENAMES]
+                paths = []
+                for i in range(0,len(dirs)):
+                    paths.append(dirs[i] + base[i])
+            elif type(paths) != list:
                 paths = [paths]
             modes = self._h[rpm.RPMTAG_FILEMODES]
             if modes:
