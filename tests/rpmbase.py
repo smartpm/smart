e2eaf01b3dd18c99d01f23a15e5a870732c88f60
Anders F Bjorklund <afb@users.sourceforge.net>
2009-12-28 13:21:23 +0100

    use realpath instead of abspath from r861, to fix test failure on darwin

--- tests/rpmbase.py
+++ tests/rpmbase.py
@@ -18,9 +18,9 @@ class getTSTest(MockerTestCase):
         #307386 is in place.
         """
         current_path = os.getcwd()
-        # Using abspath here because if it happens to be a link, the
-        # assertion below will fail.
-        test_path = os.path.abspath(self.makeDir())
+        # Using realpath here because if it happens to be a link, the
+        # assertion below will fail. (and /tmp is a symlink on Darwin)
+        test_path = os.path.realpath(self.makeDir())
         def cleanup():
             os.chdir(current_path)
             sysconf.remove("rpm-root")
