13ae1183791ca33bdc95b0cbd824e22fb3308810
Anders F Bjorklund <afb@users.sourceforge.net>
2010-03-04 18:16:30 +0100

    add test for url path merge

--- tests/slackloader.py
+++ tests/slackloader.py
@@ -1,5 +1,6 @@
-from smart.backends.slack.loader import SlackLoader, parsePackageInfo
-from smart.cache import Cache
+from smart.backends.slack.loader import SlackLoader, SlackPackageInfo, \
+                                        parsePackageInfo
+from smart.cache import Cache, Package
 
 from tests.mocker import MockerTestCase
 
@@ -31,3 +32,21 @@ class SlackLoaderTest(MockerTestCase):
         self.assertEquals(info['name'], "name2")
         self.assertEquals(info['version'], "version2-arch2-release2_slack13.0")
         self.assertEquals(info['type'], ".txz")
+
+    def test_keep_location_with_baseurl(self):
+        baseurl = "http://www.example.com/"
+        location = "example/path"
+        pkg = Package('n', 'v-a-r')
+        info = SlackPackageInfo(pkg, {'baseurl':baseurl, 'location':location,
+                                      'name':pkg.name, 'version':pkg.version })
+        url = info.getURLs()[0]
+        self.assertEquals(url, "http://www.example.com/example/path/n-v-a-r.tgz")
+
+    def test_strip_location_from_baseurl(self):
+        baseurl = "http://www.example.com/example/"
+        location = "./example/path"
+        pkg = Package('n', 'v-a-r')
+        info = SlackPackageInfo(pkg, {'baseurl':baseurl, 'location':location,
+                                      'name':pkg.name, 'version':pkg.version })
+        url = info.getURLs()[0]
+        self.assertEquals(url, "http://www.example.com/example/path/n-v-a-r.tgz")
