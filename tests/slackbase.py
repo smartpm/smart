1a897aa9ea9b57dfd1bdad85df47c3f4257f18ca
Anders F Bjorklund <afb@users.sourceforge.net>
2010-03-05 13:00:03 +0100

    add test case for slack base, just like the other backends have already

---
+++ tests/slackbase.py
@@ -0,0 +1,34 @@
+import os
+
+from mocker import MockerTestCase
+
+from smart.backends.slack.base import SlackPackage, Package, Provides
+from smart import sysconf
+
+
+class SlackPackageTest(MockerTestCase):
+
+    def test_sorting_name_takes_precedence(self):
+        pkg1 = SlackPackage("name1", "3.0-i486-1")
+        pkg2 = SlackPackage("name2", "1.0-i486-1")
+        pkg3 = SlackPackage("name3", "2.0-i486-1")
+        lst = [pkg3, pkg1, pkg2]
+        lst.sort()
+        self.assertEquals(lst, [pkg1, pkg2, pkg3])
+
+    def test_sorting_version_takes_precedence_over_release(self):
+        pkg1 = SlackPackage("name", "1.0-i486-3_slack13.0")
+        pkg2 = SlackPackage("name", "2.0-i486-2_slack13.0")
+        pkg3 = SlackPackage("name", "3.0-i486-1_slack13.0")
+        lst = [pkg3, pkg1, pkg2]
+        lst.sort()
+        self.assertEquals(lst, [pkg1, pkg2, pkg3])
+
+    def test_sorting_version_takes_precedence_over_arch(self):
+        pkg1 = SlackPackage("name", "1.0-i486-1")
+        pkg2 = SlackPackage("name", "2.0-x86_64-1")
+        pkg3 = SlackPackage("name", "3.0-i486-1")
+        lst = [pkg3, pkg1, pkg2]
+        lst.sort()
+        self.assertEquals(lst, [pkg1, pkg2, pkg3])
+
