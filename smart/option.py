1f52adfba5fe9418e72e9dd6c554e677eaf53b3e
Anders F Bjorklund <afb@users.sourceforge.net>
2012-03-08 22:54:47 +0100

    use standard optparse module

--- smart/option.py
+++ smart/option.py
@@ -19,7 +19,7 @@
 # along with Smart Package Manager; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-from smart.util import optparse
+import optparse
 from smart import Error, _
 import textwrap
 import sys, os
